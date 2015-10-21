c674f013bf80ee8319b3c5ee7c1a2188f59408ee
henriqueroco <henrique.roco@gmail.com>
2015-10-21 19:59:20 -0200

    Correções/Implementações Aluno e ObjectMessage

--- ExJms/src/main/java/br/com/feltex/academicnet/entidade/Aluno.java
+++ ExJms/src/main/java/br/com/feltex/academicnet/entidade/Aluno.java
@@ -1,5 +1,28 @@
 package br.com.feltex.academicnet.entidade;
 
-public class Aluno {
+import java.io.Serializable;
+
+public class Aluno implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private Long matricula;
+	private String nome;
+
+	public Long getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(Long matricula) {
+		this.matricula = matricula;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 
 }
--- ExJms/src/main/java/br/com/feltex/academicnet/mdb/ObjectMessage.java
+++ ExJms/src/main/java/br/com/feltex/academicnet/mdb/ObjectMessage.java
@@ -1,5 +1,16 @@
 package br.com.feltex.academicnet.mdb;
 
+import br.com.feltex.academicnet.entidade.Aluno;
+
 public class ObjectMessage {
 
+	private Aluno object;
+
+	public Aluno getObject() {
+		return object;
+	}
+
+	public void setObject(Aluno object) {
+		this.object = object;
+	}
 }
